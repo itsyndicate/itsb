b952aeb94cac08bc8237ba8382c8f03e42e18e9b
Ilya KOVALCHUK <gfalconia@live.com>
2023-08-25 11:20:03 +0200

    ITS-13: Initial git setup

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+   
+	"github.com/spf13/cobra"
+   )
 
+func main() {
+	Execute()
+}
+
+var rootCmd = &cobra.Command{
+    Use:  "itsb",
+    Short: "itsb - backup tool created by its",
+    Long: `use it fo backing up local files to S3 buckets etc`,
+    Run: func(cmd *cobra.Command, args []string) {
+
+    },
+}
+
+func Execute() {
+    if err := rootCmd.Execute(); err != nil {
+        fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
+        os.Exit(1)
+    }
 }
